d99aa197cb9cae6435279fba677b69e80e0f2709
garluna <glunawat@g.ucla.edu>
2016-04-29 16:53:51 -0700

    made data show up in drop down menus

--- php/add_MovieActor.php
+++ php/add_MovieActor.php
@@ -27,80 +27,61 @@
 
 			<div class="col-md-8">
 				<form action="add_movie.php" method="GET">
-					Movie: 	<select name="movie">
-								<?php
-									$db_connection = mysql_connect("localhost", "cs143", "");
+					<?php
+						print "Movie: 	<select name=\"mid\">";
+						$db_connection = mysql_connect("localhost", "cs143", "");
 
-								    if(!$db_connection) {
-						                $errmsg = mysql_error($db_connection);
-						                print "Connection failed: $errmsg <br />";
-						                exit(1);
-						            }
+					    if(!$db_connection) {
+			                $errmsg = mysql_error($db_connection);
+			                print "Connection failed: $errmsg <br />";
+			                exit(1);
+			            }
 
-						            mysql_select_db("CS143", $db_connection);
+			            mysql_select_db("CS143", $db_connection);
 
-						            $getMovies = "SELECT title, mid from Movie ASC";
-						            $rs = mysql_query($getMovies, $db_connection);
+			            $getMovies = "SELECT title, id FROM Movie ORDER BY title ASC;";
+			            $rs = mysql_query($getMovies, $db_connection);
 
-						            while($row = mysql_fetch_row($rs))
-						            {
-						            	$title = current($row);
-						            	next($row);
-						            	$mid = current($row);
-						            	print '<option value="$mid">$title</option>';
-						            }
+			            while($row = mysql_fetch_array($rs))
+			            {
+			            	print "<option value='" . $row['id'] . "'>" . $row['title'] . " </option>";
+			            }
 
-						            mysql_free_result($rs);
-						            mysql_close($db_connection);
-								?>
-							</select><br>
-					Actor: 	<select name="actor">
-								<option value="volvo">KT</option>
-								<option value="saab">Garima</option>
-							</select><br>
+			            mysql_free_result($rs);
+			            mysql_close($db_connection);
+
+			            print "</select><br>";
+
+			            //Select Actors
+			            print "Actor: 	<select name=\"aid\">";
+						$db_connection = mysql_connect("localhost", "cs143", "");
+
+					    if(!$db_connection) {
+			                $errmsg = mysql_error($db_connection);
+			                print "Connection failed: $errmsg <br />";
+			                exit(1);
+			            }
+
+			            mysql_select_db("CS143", $db_connection);
+
+			            $getMovies = "SELECT first, last, id FROM Actor ORDER BY first ASC;";
+			            $rs = mysql_query($getMovies, $db_connection);
+
+			            while($row = mysql_fetch_array($rs))
+			            {
+			            	print "<option value='" . $row['id'] . "'>" . $row['first'] . " " . $row['last'] . " </option>";
+			            }
+
+			            mysql_free_result($rs);
+			            mysql_close($db_connection);
+
+			            print "</select><br>";
+					?>
+					
 					Role: <input type="text" name="role"><br>
 					<input type="submit" value="Submit" />
 				</form>
 			</div>
 		</div>
-
-		<?php
-		if ( !empty( $_GET['title'])) 
-		{
-			$title = $_GET['title'];
-			$year = $_GET['year'];
-			$company = $_GET['company'];
-			$rating = $_GET['rating'];
-			$genre = $_GET['genre'];
-
-		    $db_connection = mysql_connect("localhost", "cs143", "");
-
-		    if(!$db_connection) {
-                $errmsg = mysql_error($db_connection);
-                print "Connection failed: $errmsg <br />";
-                exit(1);
-            }
-
-            mysql_select_db("CS143", $db_connection);
-
-		    $id_query = 'SELECT id FROM MaxMovieID';
-			$id_rs = mysql_query($id_query, $db_connection);
-			$row = mysql_fetch_row($id_rs);
-			$id = current($row) + 1;
-			mysql_free_result($id_rs);
-
-			$query = "INSERT INTO Movie VALUES ($id, \"$title\", \"$year\", \"$rating\", \"$company\");";
-			mysql_query($query, $db_connection);
-
-			$query2 = "INSERT INTO MovieGenre VALUES ($id, \"$genre\");";
-			mysql_query($query2, $db_connection);
-
-			$increment_query = "UPDATE MaxMovieID SET id=$id;"; // TACO write a query to increment max ID
-			mysql_query($increment_query, $db_connection);
-			
-			mysql_close($db_connection);
-        }
-        	
-		?>
 	</body>
 </html>
