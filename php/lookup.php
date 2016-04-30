c1d2ca84d2cca59b3cbd1c2281339a73859c30a5
garluna <glunawat@g.ucla.edu>
2016-04-30 02:04:52 -0700

    displayed actor info for show_actor

--- php/lookup.php
+++ php/lookup.php
@@ -12,25 +12,31 @@
         mysql_select_db("CS143", $db_connection);
 
 		// Print information about the actor/actress
-		print '<h3>Show Actor Info</h3>';
+		print '<h3>Actor Info</h3>';
 		$actor_info_q = "SELECT first, last, sex, dob, dod FROM Actor WHERE id=$aid";
 
 		$ainfo_rs = mysql_query($actor_info_q, $db_connection);
-		$row = mysql_fetch_row($ainfo_rs);
-		print "Name: " 			. $row['first'] . " " . $row['last'];
-		print "Gender: " 		. $row['sex'];
-		print "Date of Birth: " . $row['dob'];
+
+		$row = mysql_fetch_assoc($ainfo_rs);
+		// print_r($row);
+		print "Name: " 			. $row['first'] . " " . $row['last'] . "<br>";
+		print "Gender: " 		. $row['sex'] . "<br>";
+		print "Date of Birth: " . $row['dob'] . "<br>";
 		print "Date of Death: " . $row['dod'];
 		mysql_free_result($id_rs);
 
 		// Print films they've played a role in
 		print '<h3>Filmography</h3>';
 		$film_info_q = "SELECT role, title FROM MovieActor, Movie WHERE MovieActor.mid = Movie.id AND MovieActor.aid = $aid";
+
 		$finfo_rs = mysql_query($film_info_q, $db_connection);
 
 		while($row2 = mysql_fetch_array($finfo_rs))
 		{
-			print "Played role: " . $row2['role'] . "in: " . $row2['title']; 
+			print "<b>Played role: </b>" . $row2['role'] . "<b> in: </b>" . $row2['title'] . "<br>"; 
 		}
+
+		mysql_free_result($finfo_rs);
+		mysql_close($db_connection);
 	}
 ?>
