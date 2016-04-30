d24dd8e8f561ccea68775f97ce08c894031943f3
garluna <glunawat@g.ucla.edu>
2016-04-30 14:55:40 -0700

    added files for movie information display

--- php/lookup.php
+++ php/lookup.php
@@ -13,7 +13,7 @@
 
 		// Print information about the actor/actress
 		print '<h3>Actor Info</h3>';
-		$actor_info_q = "SELECT first, last, sex, dob, dod FROM Actor WHERE id=$aid";
+		$actor_info_q = "SELECT first, last, sex, dob, dod FROM Actor WHERE id=$aid;";
 
 		$ainfo_rs = mysql_query($actor_info_q, $db_connection);
 
@@ -23,7 +23,7 @@
 		print "Gender: " 		. $row['sex'] . "<br>";
 		print "Date of Birth: " . $row['dob'] . "<br>";
 		print "Date of Death: " . $row['dod'];
-		mysql_free_result($id_rs);
+		mysql_free_result($ainfo_rs);
 
 		// Print films they've played a role in
 		print '<h3>Filmography</h3>';
@@ -39,4 +39,57 @@
 		mysql_free_result($finfo_rs);
 		mysql_close($db_connection);
 	}
+
+	function movieLookup($mid)
+	{
+		$db_connection = mysql_connect("localhost", "cs143", "");
+
+	    if(!$db_connection) {
+            $errmsg = mysql_error($db_connection);
+            print "Connection failed: $errmsg <br />";
+            exit(1);
+        }
+
+        mysql_select_db("CS143", $db_connection);
+
+		// Print information about the actor/actress
+		print '<h3>Movie Info</h3>';
+		$movie_info_q = "SELECT title, year, rating, company FROM Actor WHERE id=$mid;";
+		$move_genre_q = "SELECT genre FROM MovieGenre WHERE mid=$mid;";
+
+		$minfo_rs = mysql_query($movie_info_q, $db_connection);
+		$mgenre_rs = mysql_query($movie_genre_q, $db_connection);
+
+		$row = mysql_fetch_assoc($minfo_rs);
+		// print_r($row);
+		print "Title: "		. $row['title'] . " (" . $row['year'] . ")<br>";
+		print "Producer: " 	. $row['company'] . "<br>";
+		print "Rating: " 	. $row['rating'] . "<br>";
+		print "Genre: ";
+		if ($row2 = mysql_fetch_array($mgenre_rs))
+		{
+			print $row2['genre']
+		}
+		while ($row2 = mysql_fetch_array($mgenre_rs))
+		{
+			print " ," . $row2['genre'];
+		}
+		print "<br>";
+		mysql_free_result($minfo_rs);
+		mysql_free_result($mgenre_rs);
+
+		// Print films they've played a role in
+		print '<h3>Actors in thie Movie</h3>';
+		$actor_info_q = "SELECT first, last, role FROM MovieActor, Actor WHERE MovieActor.mid = $mid AND MovieActor.aid = Actor.id";
+
+		$ainfo_rs = mysql_query($actor_info_q, $db_connection);
+
+		while($row3 = mysql_fetch_array($ainfo_rs))
+		{
+			print "<b>Actor: </b>" . $row3['first'] . " ". $row3['last'] . "<b> as </b>" . $row3['role'] . "<br>"; 
+		}
+
+		mysql_free_result($ainfo_rs);
+		mysql_close($db_connection);
+	}	
 ?>
---
+++ php/show_movie.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CS143 Project 1B</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+		<h1>CS143 Project 1B: Movie Database</h1>
+		<h3>Garima Lunawat and Katie Aspinwall</h3>
+
+		<div class="row">
+			<div class="col-md-4">
+				<p>Add Actor / Director</p>
+				<p>Add Movie Information</p>
+				<p>Add Movie / Actor Relation</p>
+				<p>Add Movie / Director Relation</p>
+				<br>
+				<p>Actor Information</p>
+				<p>Movie Information</p>
+				<br>
+				<p>Search</p>
+		
+			</div> 
+
+			<div class="col-md-8">
+					<?php
+						include 'lookup.php';
+
+						$db_connection = mysql_connect("localhost", "cs143", "");
+
+					    if(!$db_connection) {
+					    	echo "Hi";
+			                $errmsg = mysql_error($db_connection);
+			                print "Connection failed: $errmsg <br />";
+			                exit(1);
+			            }
+
+			            mysql_select_db("CS143", $db_connection);
+
+			            $id_query = "SELECT MIN(id) FROM Movie;";
+						$id_rs = mysql_query($id_query, $db_connection);
+						$row = mysql_fetch_assoc($id_rs);
+						$minID = current($row);
+						// print_r($row);
+						
+			            // actorLookup($minID);
+			            movieLookup($minID);
+			            mysql_free_result($id_rs);
+			            mysql_close($db_connection);
+					?>
+			</div>
+		</div>
+	</body>
+</html>
